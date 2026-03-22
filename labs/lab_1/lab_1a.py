280bed432bd120fcaa881dd15325aaf8ff429169
Justen Mao <justenmao@gmail.com>
2026-03-22 17:06:54 -0500

    add introduction print

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Justen Mao" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hello! My name is Justen Mao, and I'm a junior at Lovejoy High School in Texas. I do VEX Robotics, and am going to Worlds in about a month for the 4th year in a row.")
 if __name__ == "__main__":
     main()
